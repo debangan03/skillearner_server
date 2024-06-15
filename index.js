567906ef68f90a97e42648ec0fd2aececfb0c477
Debangan Bhattacharyya <112092614+debangan03@users.noreply.github.com>
2024-06-15 12:38:41 +0530

    final commit

--- index.js
+++ index.js
@@ -103,7 +103,7 @@ app.post("/forgotpassword", async (req, res) => {
     await user.save();
 
     // Generate reset link
-    const resetLink = `http://localhost:5173/resetpassword/${token}`;
+    const resetLink = `https://skillearner.netlify.app/resetpassword/${token}`;
 
     res.status(200).json({ resetLink });
   } catch (error) {
